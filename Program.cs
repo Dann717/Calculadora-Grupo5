8d129ce782396b352b799264d37098c45f4108f1
Bryan Caal <bcaals1@miumg.edu.gt>
2025-08-03 10:34:49 -0600

    Agregue la funcion restar

--- Program.cs
+++ Program.cs
@@ -20,8 +20,12 @@ class Program
         {
             Sumar();
         }
+        else if (opcion == 2)
+        {
+            Restar();
+        }
 
-            // TODO: Implementar funciones de suma, resta, multiplicación, división
+        // TODO: Implementar funciones de suma, resta, multiplicación, división
     }
     static void Sumar()
     {
@@ -31,6 +35,15 @@ class Program
         double num2 = double.Parse(Console.ReadLine());
         double resultado = num1 + num2;
         Console.WriteLine($"El resultado de la suma es: {resultado}");
+    }
+        static void Restar()
+    {
+        Console.Write("Ingrese el primer número: ");
+        double num1 = double.Parse(Console.ReadLine());
+        Console.Write("Ingrese el segundo número: ");
+        double num2 = double.Parse(Console.ReadLine());
+        double resultado = num1 - num2;
+        Console.WriteLine($"El resultado de la resta es: {resultado}");
     }
 
 }
