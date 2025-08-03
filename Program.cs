cee5e810853082f82c8bf02d5c7240fdb0a2f45b
misaelbmw1 <wmazb@miumg.edu.gt>
2025-08-03 12:42:22 -0600

    agregué la función de multiplicación

--- Program.cs
+++ Program.cs
@@ -24,6 +24,12 @@ class Program
         {
             Restar();
         }
+
+        else if (opcion ==3)
+        {
+            Multiplicacion();
+        }
+
         else if (opcion ==4)
         {
             Division();
@@ -48,6 +54,15 @@ class Program
         double num2 = double.Parse(Console.ReadLine());
         double resultado = num1 - num2;
         Console.WriteLine($"El resultado de la resta es: {resultado}");
+    }
+    static void Multiplicacion()
+    {
+        Console.Write("Ingrese el primer número: ");
+        double num1 = double.Parse(Console.ReadLine());
+        Console.Write("Ingrese el segundo número: ");
+        double num2 = double.Parse(Console.ReadLine());
+        double resultado = num1 * num2;
+        Console.WriteLine($"El resultado de la suma es: {resultado}");
     }
     static void Division()
     {
